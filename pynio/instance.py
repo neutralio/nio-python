9af73294c3802dcaf325dbe9e8164526c50123c6
Garrett Berg <gberg@neutral.io>
2015-02-18 09:46:40 -0700

    added convienience functions for creating and adding blocks and services

--- pynio/instance.py
+++ pynio/instance.py
@@ -1,4 +1,4 @@
-from .rest import REST
+from .rest import REST, Request
 from .block import Block
 from .service import Service
 from .progress import ProgressBar
@@ -7,6 +7,7 @@ from .progress import ProgressBar
 class Instance(REST):
     """ Interface for a running nio instance.
     """
+    print_function = print
 
     def __init__(self, host='127.0.0.1', port=8181, creds=None):
         super().__init__(host, port, creds)
@@ -49,24 +50,38 @@ class Instance(REST):
                                   instance=self)
         return services
 
+    def create_block(self, name, type, config=None):
+        '''Convenience function to create a block and add it to instance'''
+        return Block(name, type, config, instance=self)
+
+    def create_service(self, name, type=None, config=None):
+        '''Convenience function to create a service and add it to instance'''
+        if type is None:
+            return Service(name, config=config, instance=self)
+        else:
+            return Service(name, type, config=config, instance=self)
+
     def start(self, services):
-        '''Start an iterator of services as fast as possible'''
-        for s in ProgressBar('Starting:')(services):
-            s.start()
+        '''Start an iterator of services. This is designed to ensure that
+        services start as quickly as possible, even under heavy loads'''
+        for s in ProgressBar('Starting:', print=self.print_function)(services):
+            Request(s, s.start).complete('started')
 
     def stop(self, services):
         '''Stop an iterator of services as fast as possible'''
-        for s in ProgressBar('Stopping:')(services):
+        for s in ProgressBar('Stopping:', print=self.print_function)(services):
             s.stop()
 
     def DELETE_ALL(self):
         '''Deletes all blocks and services from an instance
-        regardless of whether or not they can be loaded and does
-        a reset'''
+        regardless of whether or not they can be loaded. Does a reset after
+        '''
         blocks, services = self._get('blocks'), self._get('services')
-        for b in blocks:
+        for b in ProgressBar("Deleting Blocks", print=self.print_function)(
+                blocks):
             self._delete('blocks/{}'.format(b))
-        for s in services:
+        for s in ProgressBar("Deleting Services", print=self.print_function)(
+                services):
             self._get('services/{}/stop'.format(s))
             self._delete('services/{}'.format(s))
         self.reset()
